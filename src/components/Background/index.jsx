48b95407ae43590e2f00203e4b12e7a34fb1859c
Cleyton Jesus <cleytonjesus007@gmail.com>
2023-07-03 11:46:10 -0300

    Setting which background to show depending on the width of the screen

--- src/components/Background/index.jsx
+++ src/components/Background/index.jsx
@@ -1,10 +1,14 @@
+import { useState } from "react";
 import { useEffect } from "react";
 import { useRef } from "react";
 
 const videoSrc = "assets/video/background/bg_psn.mp4";
+const imageSrc = "assets/images/bg_pic.jpg";
 
 export default function Background({ mainRef }) {
+    const [backup, setBackup] = useState(false)
     const videoBgRef = useRef();
+
     const configVideo = () => {
         videoBgRef.current.style.width = "100%"
         videoBgRef.current.style.height = "100vh"
@@ -14,12 +18,35 @@ export default function Background({ mainRef }) {
         videoBgRef.current.muted = true;
         return;
     }
-    useEffect(() => document.body.onload = () => configVideo(), [])
+    useEffect(() => {
+        if (window.innerWidth <= 768) {
+            setBackup(true);
+            return;
+        }
+        document.body.onload = () => configVideo()
+    }, [])
+
+    useEffect(() => mainRef?.current.setAttribute("class", "show"), [backup])
+
+    if (backup) {
+        return (
+            <div className="fixed -z-10 top-0 left-0 right-0 bottom-0  w-full h-screen showBg">
+                <img className="w-full h-full object-cover" src={imageSrc} />
+            </div>
+        )
+    }
+
+
     return (
         <video
             ref={videoBgRef}
             className="fixed top-0 bottom-0 right-0 left-0 -z-10 invisible showBg"
             onCanPlay={() => mainRef?.current.setAttribute("class", "show")}
+            onError={() => setBackup(true)}
+            onAbort={() => setBackup(true)}
+            onStalled={() => setBackup(true)}
+            onEmptied={() => setBackup(true)}
+
         >
             <source src={videoSrc} type="video/mp4"></source>
         </video>
