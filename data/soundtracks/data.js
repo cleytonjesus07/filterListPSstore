5388d6e8137d569ba4b4b364342806ae76044312
Cleyton Jesus <cleytonjesus007@gmail.com>
2023-07-03 20:05:38 -0300

    change mk cover soundtrack

--- data/soundtracks/data.js
+++ data/soundtracks/data.js
@@ -1,7 +1,7 @@
 export default [
     {
         id: "soundtrack1",
-        cover: "https://th.bing.com/th/id/R.726547495cfad3c0a419c4b5b6d0f5af?rik=jIc%2bsgRi%2fENeUg&riu=http%3a%2f%2forig03.deviantart.net%2f5d0f%2ff%2f2014%2f150%2f1%2f3%2fnew_mortal_kombat_symbol_by_yurtigo-d7kbt25.png&ehk=qHrN5RiqZC9vakXI37YBAiknHr4HxOISleL6EY4Q%2btI%3d&risl=&pid=ImgRaw&r=0",
+        cover: "https://i.ytimg.com/vi/nW28UpPqZMw/maxresdefault.jpg",
         title: "Mortal Kombat Theme Song",
         type: "Soundtrack",
         platform: "PS4/PS5",
--- src/components/Cart/FooterCart/index.jsx
+++ src/components/Cart/FooterCart/index.jsx
@@ -5,8 +5,8 @@ export default function FooterCart() {
     const amount = useCartStore((state) => state.amount);
     return (
         <div className="py-5 flex gap-2 items-center justify-between border-t-2">
-            <span className="font-bold">Valor:</span>
+            < span className="font-bold" > Valor:</span >
             <span>{formatedPrice(amount)}</span>
-        </div>
+        </div >
     )
 }
